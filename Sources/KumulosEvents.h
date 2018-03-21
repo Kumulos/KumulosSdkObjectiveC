0036b6f15657f0ae331f6e05a8c29d6bdb2d0f39
Christopher Wyllie <c.wyllie@kumulos.com>
2018-03-21 15:15:42 +0000

    Add consts for Kumulos event identifiers

---
+++ Sources/KumulosEvents.h
@@ -0,0 +1,12 @@
+//
+//  KumulosEvents.h
+//  KumulosSDK
+//
+//  Created by cgwyllie on 21/03/2018.
+//
+
+#ifndef KumulosEvents_h
+#define KumulosEvents_h
+
+
+#endif /* KumulosEvents_h */
---
+++ Sources/KumulosEvents.m
@@ -0,0 +1,8 @@
+//
+//  KumulosEvents.m
+//  KumulosSDK
+//
+//  Created by cgwyllie on 21/03/2018.
+//
+
+#import <Foundation/Foundation.h>
