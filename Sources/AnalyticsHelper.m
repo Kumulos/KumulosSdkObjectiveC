d30329002e573fb56b5ca81ac9ed6f9f167b480d
Christopher Wyllie <c.wyllie@kumulos.com>
2019-01-28 18:02:13 +0000

    Use performBlock helpers to ensure NSManagedObject access is always on correct thread
    Also passes object IDs instead of instances to the prune method.

--- Sources/AnalyticsHelper.m
+++ Sources/AnalyticsHelper.m
@@ -123,8 +123,9 @@
     }
     
     self.analyticsContext = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSPrivateQueueConcurrencyType];
-    
-    self.analyticsContext.persistentStoreCoordinator = storeCoordinator;
+    [self.analyticsContext performBlockAndWait:^{
+        self.analyticsContext.persistentStoreCoordinator = storeCoordinator;
+    }];
 }
 
 - (void) registerListeners {
@@ -206,27 +207,32 @@
 }
 
 - (void) syncEvents {
-    NSArray<KSEventModel*>* results = [self fetchEventsBatch];
-    
-    if (results.count) {
-        [self syncEventsBatch:results];
-    }
-    else if (self.bgTask != UIBackgroundTaskInvalid) {
-        [[UIApplication sharedApplication] endBackgroundTask:self.bgTask];
-        self.bgTask = UIBackgroundTaskInvalid;
-    }
+    [self.analyticsContext performBlockAndWait:^{
+        NSArray<KSEventModel*>* results = [self fetchEventsBatch];
+
+        if (results.count) {
+            [self syncEventsBatch:results];
+        }
+        else if (self.bgTask != UIBackgroundTaskInvalid) {
+            [[UIApplication sharedApplication] endBackgroundTask:self.bgTask];
+            self.bgTask = UIBackgroundTaskInvalid;
+        }
+    }];
 }
 
 - (void) syncEventsBatch:(NSArray<KSEventModel*>*) events {
-    NSMutableArray* data = [[NSMutableArray alloc] initWithCapacity:events.count];
+    NSMutableArray* data = [NSMutableArray arrayWithCapacity:events.count];
+    NSMutableArray<NSManagedObjectID*>* eventIds = [NSMutableArray arrayWithCapacity:events.count];
     
     for (KSEventModel* event in events) {
         [data addObject:[event asDict]];
+        [eventIds addObject:event.objectID];
     }
     
     NSString* path = [NSString stringWithFormat:@"/v1/app-installs/%@/events", [Kumulos installId]];
+
     [self.kumulos.eventsHttpClient post:path data:data onSuccess:^(NSHTTPURLResponse * _Nullable response, id  _Nullable decodedBody) {
-        NSError* err = [self pruneEventsBatch:events];
+        NSError* err = [self pruneEventsBatch:eventIds];
         
         if (err) {
             NSLog(@"Failed to prune events: %@", err);
@@ -243,15 +249,18 @@
     }];
 }
 
-- (NSError*) pruneEventsBatch:(NSArray<KSEventModel*>*) events {
-    NSError* err = nil;
+- (NSError*) pruneEventsBatch:(NSArray<NSManagedObjectID*>*) eventIds {
+    __block NSError* err = nil;
 
-    for (KSEventModel* event in events) {
-        [self.analyticsContext deleteObject:event];
-    }
-    
-    [self.analyticsContext save:&err];
-    
+    [self.analyticsContext performBlockAndWait:^{
+        for (NSManagedObjectID* eventId in eventIds) {
+            KSEventModel* event = [self.analyticsContext objectWithID:eventId];
+            [self.analyticsContext deleteObject:event];
+        }
+
+        [self.analyticsContext save:&err];
+    }];
+
     return err;
 }
 
