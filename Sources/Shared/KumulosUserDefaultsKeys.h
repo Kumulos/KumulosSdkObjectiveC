a720555f51d96d27935d088f43fff229467173f3
vvoicehovics <vladislav.voicehovich@gmail.com>
2020-03-27 10:43:29 +0000

    port KumulosUserDefaultsKey

---
+++ Sources/Shared/KumulosUserDefaultsKeys.h
@@ -0,0 +1,26 @@
+//
+//  KumulosUserDefaultsKeys.h
+//  KumulosSDK iOS
+//
+//  Created by Vladislav Voicehovics on 27/03/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString* const KumulosApiKey;
+extern NSString* const KumulosSecretKey;
+extern NSString* const KumulosInstallUUID;
+extern NSString* const KumulosUserID;
+extern NSString* const KumulosBadgeCount;
+
+//exist only in standard defaults for app
+extern NSString* const KumulosMigratedToGroups;
+extern NSString* const KumulosMessagesLastSyncTime;
+extern NSString* const KumulosInAppConsented;
+
+//exist only in standard defaults for extension
+extern NSString* const KumulosDynamicCategory;
+
+
+extern NSString * const SharedDefaultsKeys[];
+
---
+++ Sources/Shared/KumulosUserDefaultsKeys.m
@@ -0,0 +1,31 @@
+//
+//  KumulosUserDefaultsKeys.m
+//  KumulosSDK iOS
+//
+//  Created by Vladislav Voicehovics on 27/03/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+
+#import "KumulosUserDefaultsKeys.h"
+
+NSString* const KumulosApiKey = @"KumulosApiKey";
+NSString* const KumulosSecretKey = @"KumulosSecretKey";
+NSString* const KumulosInstallUUID = @"KumulosUUID";
+NSString* const KumulosUserID = @"KumulosCurrentUserID";
+NSString* const KumulosBadgeCount = @"KumulosBadgeCount";
+
+//exist only in standard defaults for app
+NSString* const KumulosMigratedToGroups = @"KumulosDidMigrateToAppGroups";
+NSString* const KumulosMessagesLastSyncTime = @"KumulosMessagesLastSyncTime";
+NSString* const KumulosInAppConsented = @"KumulosInAppConsented";
+
+//exist only in standard defaults for extension
+NSString* const KumulosDynamicCategory = @"__kumulos__dynamic__categories__";
+
+
+NSString * const SharedDefaultsKeys[] =  { KumulosApiKey, KumulosSecretKey, KumulosInstallUUID, KumulosUserID, KumulosBadgeCount };
+
+
+
