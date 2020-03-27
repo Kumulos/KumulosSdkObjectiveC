f58779fed440686a32994b50310b2039b6d657cd
vvoicehovics <vladislav.voicehovich@gmail.com>
2020-03-27 13:27:39 +0000

    port KumulosHelper

---
+++ Sources/Shared/KumulosHelper.h
@@ -0,0 +1,14 @@
+//
+//  KumulosHelper.h
+//  KumulosSDK iOS
+//
+//  Created by Vladislav Voicehovics on 27/03/2020.
+//
+
+@interface KumulosHelper : NSObject
+
++ (NSString* _Nonnull) installId;
++ (NSString* _Nonnull) currentUserIdentifier;
+
+
+@end
---
+++ Sources/Shared/KumulosHelper.m
@@ -0,0 +1,44 @@
+//
+//  KumulosHelper.m
+//  KumulosSDK iOS
+//
+//  Created by Vladislav Voicehovics on 27/03/2020.
+//
+
+#import <Foundation/Foundation.h>
+#import "KumulosHelper.h"
+#import "KumulosUserDefaultsKeys.h"
+
+
+static NSString* _Nonnull const userIdLocker = @"";
+
+@implementation KumulosHelper
+
++ (NSString*) installId {
+    @synchronized (self) {
+        NSString* installId = [[NSUserDefaults standardUserDefaults] objectForKey:KumulosInstallUUID];
+        
+        if (!installId) {
+            installId = [[[NSUUID UUID] UUIDString] lowercaseString];
+            [[NSUserDefaults standardUserDefaults] setObject:installId forKey:KumulosInstallUUID];
+            [[NSUserDefaults standardUserDefaults] synchronize];
+        }
+        
+        return installId;
+    }
+}
+
+
++ (NSString*) currentUserIdentifier {
+    @synchronized (userIdLocker) {
+        NSString* userId = [NSUserDefaults.standardUserDefaults objectForKey:KumulosUserID];
+        if (userId) {
+            return userId;
+        }
+    }
+
+    return KumulosHelper.installId;
+}
+
+
+@end
