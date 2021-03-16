5a2a130e39a6f28166d170ae81262f3df65a56ad
vvoicehovics <vladislav.voicehovich@gmail.com>
2021-03-16 10:46:29 +0200

    move method into pushInit

--- Sources/Kumulos+Push.m
+++ Sources/Kumulos+Push.m
@@ -71,6 +71,15 @@ void kumulos_applicationDidReceiveRemoteNotificationFetchCompletionHandler(id se
 @implementation Kumulos (Push)
 
 - (void) pushInit {
+    
+#if TARGET_OS_IOS
+    if (@available(iOS 10.0, *)) {
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 5 * NSEC_PER_SEC), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^() {
+            [self maybeTrackPushDismissedEvents];
+        });
+    }
+#endif
+    
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         Class class = UIApplication.sharedApplication.delegate.class;
